f70a904c3bb5446194d23d02b5e0993ff681e94f
Acrata <hxtr227@gmail.com>
2023-05-10 21:25:18 +0200

    Devtols pildora presentation

---
+++ src/script.js
@@ -0,0 +1,9 @@
+console.log('auuu')
+console.log(
+        "%c 💊 F5 | Pildora Devtools | P7M",	
+        "color:gold;font-family:system-ui;font-size:2rem;font-weight:bold"
+      );
+console.log(
+        "%c 🤖 Coder Hector",	
+        "color:aliceblue;font-family:system-ui;font-size:1rem;font-weight:bold"
+      );
